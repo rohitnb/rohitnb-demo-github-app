77ad0126aa0f5e7a60dd348a429e3b7d23d907a4
Rohit N B <48172220+rohitnb@users.noreply.github.com>
2022-04-03 18:17:58 +0000

    template ready

--- index.js
+++ index.js
@@ -6,12 +6,18 @@ module.exports = (app) => {
   // Your code here
   app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
+  app.on(["issues.edited","issues.opened"] , async (context) => {
+    //console.log("Context is "+JSON.stringify(context))
+    console.log("\nAction: Issue Opened or Edited \nIssue Number: "+context.payload.issue.number+"\nIssue Title:"+context.payload.issue.title)
+    // const issueComment = context.issue({
+    //   body: "Thanks for opening this issue!",
+    // });
+    // return context.octokit.issues.createComment(issueComment);
   });
+
+  app.on(["pull_request.opened", "pull_request.edited"], async(context) => {
+    console.log("\nAction: PR Opened or Edited \nPR Number: "+context.payload.pull_request.number+"\nPR Title:"+context.payload.pull_request.title)
+  })
 
   // For more information on building apps:
   // https://probot.github.io/docs/
